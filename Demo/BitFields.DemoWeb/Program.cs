00f83914c9c05a8dca266ed084eef0c1d570d9e6
dhadner <93440039+dhadner@users.noreply.github.com>
2026-03-04 20:16:52 -0800

    Fix demo deployment to use correct version of NuGet package.

--- Demo/BitFields.DemoWeb/Program.cs
+++ Demo/BitFields.DemoWeb/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var host = builder.Build();
 
 // Clear the crash-detection flag set by index.html. If we reach this point,
-// the .NET WASM runtime loaded successfully and the app is running.
+// the .NET WASM runtime loaded successfully and the app is running. 
 var js = host.Services.GetRequiredService<IJSRuntime>();
 await js.InvokeVoidAsync("localStorage.removeItem", "blazorBoot");
 
